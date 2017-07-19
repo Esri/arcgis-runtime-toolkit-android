44a88298bdd1c17e08f259fd7495c6c4543c1690
Dan O'Neill <doneill@esri.com>
2017-07-19 00:11:54 -0400

    add doclava templates

---
+++ doclava/templates/head_tag.cs
@@ -0,0 +1,65 @@
+<head>
+<meta charset="UTF-8">
+<meta name="last-modified" content="<?cs var:date ?>">
+<meta name="product" content="api-for-android">
+<meta name="version" content="100.1.0">
+<meta name="subject" content="DEVELOPER,MOBILE,ARCGIS RUNTIME">
+<meta name="sub_category" content="Runtime SDK for Android">
+<meta name="sub_category_label" content="ArcGIS Android">
+<meta name="search-collection" content="help">
+<meta name="content_type" content="API Reference">
+<meta name="content_type_label" content="API Reference">
+<meta name="search-category" content="help-api,help-api-ref">
+<meta name="subject" content="MOBILE">
+<meta name="subject_label" content="MOBILE">
+<meta name="content-language" content="en">
+<?cs if:project.name ?>
+  <meta name="description" content="Javadoc API documentation for <?cs var:project.name ?>." />
+<?cs else ?>
+  <meta name="description" content="Javadoc API documentation." />
+<?cs /if ?>
+<link rel="shortcut icon" type="image/x-icon" href="<?cs var:toroot ?>favicon.ico" />
+<title>
+<?cs if:page.title ?>
+  <?cs var:page.title ?>
+<?cs /if ?>
+<?cs if:project.name ?>
+| <?cs var:project.name ?>
+<?cs /if ?>
+</title>
+<link href="<?cs var:toassets ?>doclava-developer-docs.css" rel="stylesheet" type="text/css" />
+<link href="<?cs var:toassets ?>customizations.css" rel="stylesheet" type="text/css" />
+<script src="<?cs var:toassets ?>search_autocomplete.js" type="text/javascript"></script>
+<script src="<?cs var:toassets ?>jquery-resizable.min.js" type="text/javascript"></script>
+<script src="<?cs var:toassets ?>doclava-developer-docs.js" type="text/javascript"></script>
+<script src="<?cs var:toassets ?>prettify.js" type="text/javascript"></script>
+<script type="text/javascript">
+  setToRoot("<?cs var:toroot ?>", "<?cs var:toassets ?>");
+</script><?cs
+if:reference ?>
+<script src="<?cs var:toassets ?>doclava-developer-reference.js" type="text/javascript"></script>
+<script src="<?cs var:toassets ?>navtree_data.js" type="text/javascript"></script><?cs
+/if ?>
+<script src="<?cs var:toassets ?>customizations.js" type="text/javascript"></script>
+<noscript>
+  <style type="text/css">
+    html,body{overflow:auto;}
+    #body-content{position:relative; top:0;}
+    #doc-content{overflow:visible;border-left:3px solid #666;}
+    #side-nav{padding:0;}
+    #side-nav .toggle-list ul {display:block;}
+    #resize-packages-nav{border-bottom:3px solid #666;}
+  </style>
+</noscript>
+<!-- Google Analytics -->
+<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-215788-63', 'arcgis.com');
+ga('send', 'pageview');</script>
+<!-- End Google Analytics -->
+<!-- Adobe Analytics head code include -->
+<script src="//assets.adobedtm.com/2e9cd01e19dc5ac4867e752f17a2f1ea1923e5af/satelliteLib-0ed1001fd441a838aefe8e755be42aaafddcc46b.js"></script>
+<!-- END: Adobe Analytics head code include -->
+</head>
---
+++ doclava/templates/index.cs
@@ -0,0 +1,20 @@
+<html>
+<head>
+  <meta name="last-modified" content="@date@">
+  <meta name="product" content="api-for-android">
+  <meta name="version" content="100.1.0">
+  <meta name="subject" content="DEVELOPER,MOBILE,ARCGIS RUNTIME">
+  <meta name="sub_category" content="Runtime SDK for Android">
+  <meta name="sub_category_label" content="ArcGIS Android">
+  <meta name="search-collection" content="help">
+  <meta name="content_type" content="API Reference">
+  <meta name="content_type_label" content="API Reference">
+  <meta name="search-category" content="help-api,help-api-ref">
+  <meta name="subject" content="MOBILE">
+  <meta name="subject_label" content="MOBILE">
+  <meta name="content-language" content="en">
+</head>``
+<body>``
+<?cs include:"analytics.cs" ?>
+</body>
+</html>
