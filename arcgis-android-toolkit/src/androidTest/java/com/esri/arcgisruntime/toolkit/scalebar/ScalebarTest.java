331f8932d2e74f8a6d0249f4fe5e4a1d044b0561
Alan Lucas <alucas@esri.com>
2017-08-22 12:40:02 +0100

    Scalebar: fixed the package that ScalebarTest is in

--- arcgis-android-toolkit/src/androidTest/java/com/esri/arcgisruntime/toolkit/scalebar/ScalebarTest.java
+++ arcgis-android-toolkit/src/androidTest/java/com/esri/arcgisruntime/toolkit/scalebar/ScalebarTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.esri.arcgisruntime.toolkit.com.esri.arcgisruntime.toolkit.scalebar;
+package com.esri.arcgisruntime.toolkit.scalebar;
 
 import android.content.Context;
 import android.graphics.Color;
