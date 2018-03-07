da7c9a3b8abd9b09286da33f8eb45e652e28881e
Alan Lucas <alucas@esri.com>
2018-03-07 15:38:51 +0000

    Start fleshing out CompassTestActivity - work in progress

--- toolkit-test-app/src/main/java/com/esri/arcgisruntime/toolkit/test/compass/CompassTestActivity.java
+++ toolkit-test-app/src/main/java/com/esri/arcgisruntime/toolkit/test/compass/CompassTestActivity.java
@@ -17,6 +17,13 @@ package com.esri.arcgisruntime.toolkit.test.compass;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import com.esri.arcgisruntime.ArcGISRuntimeException;
+import com.esri.arcgisruntime.loadable.LoadStatus;
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.MapView;
+import com.esri.arcgisruntime.toolkit.compass.Compass;
 import com.esri.arcgisruntime.toolkit.test.R;
 
 /**
@@ -24,10 +31,58 @@ import com.esri.arcgisruntime.toolkit.test.R;
  */
 public final class CompassTestActivity extends AppCompatActivity {
 
+  private static final String TAG = CompassTestActivity.class.getSimpleName();
+
+  private MapView mMapView;
+
+  private ArcGISMap mMap;
+
+  private Compass mCompass;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
+    // Create a map containing just a Basemap
+    mMap = new ArcGISMap(Basemap.createStreetsVector());
+
+    // Set the content view, find the MapView within it and set the map created above on the MapView
     setContentView(R.layout.compass_test);
+    mMapView = findViewById(R.id.mapview);
+    mMapView.setMap(mMap);
+
+    // Setting the map on the MapView causes the map to be loaded; set a DoneLoading listener
+    mMap.addDoneLoadingListener(new Runnable() {
+      @Override
+      public void run() {
+        // Log error information if the map's not loaded successfully
+        if (mMap.getLoadStatus() != LoadStatus.LOADED) {
+          logLoadError(mMap.getLoadError());
+        }
+
+        // Create a Compass and add it to the MapView (Workflow 1)
+        mCompass = new Compass(mMapView.getContext());
+        mCompass.addToMapView(mMapView);
+      }
+    });
   }
+
+  /**
+   * Writes information about a load error to the log.
+   *
+   * @param loadError the load error
+   */
+  private void logLoadError(ArcGISRuntimeException loadError) {
+    if (loadError != null) {
+      Log.e(TAG, String.format("loadError.getMessage: %s", loadError.getMessage()));
+      Log.e(TAG, String.format("loadError.getAdditionalMessage: %s", loadError.getAdditionalMessage()));
+      Log.e(TAG, String.format("loadError.getErrorDomain: %s", loadError.getErrorDomain()));
+      Log.e(TAG, "loadError.getErrorCode: " + loadError.getErrorCode());
+      if (loadError.getCause() != null) {
+        Log.e(TAG, String.format("loadError.getCause().getMessage: %s", loadError.getCause().getMessage()));
+        loadError.getCause().printStackTrace();
+      }
+    }
+  }
+
 }
