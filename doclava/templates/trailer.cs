1dd2dfddc40b6dbb320b7fb639ef634d7ea5e529
Luke William Duncan <lduncan@esri.com>
2019-04-09 14:46:23 +0100

    Removed `doclava` dir.

--- doclava/templates/trailer.cs
+++
@@ -1,7 +0,0 @@
-</div> <!-- end body-content --> <?cs # normally opened by header.cs ?>
-
-<script type="text/javascript">
-init(); /* initialize doclava-developer-docs.js */
-</script>
-<script type="text/javascript">if (_satellite && _satellite.pageBottom) { _satellite.pageBottom(); }</script>
-<script type="text/javascript" src="https://go.pardot.com/dcjs/82202/1182/dc.js"></script>
