f34c76331689e9f7400af58e7cd24d14fe540e45
Luke William Duncan <lduncan@esri.com>
2019-04-26 08:46:08 +0100

    Refactored `GraduatedLineRenderer`

--- arcgis-android-toolkit/src/main/java/com/esri/arcgisruntime/toolkit/scalebar/style/renderer/GraduatedLineRenderer.kt
+++ arcgis-android-toolkit/src/main/java/com/esri/arcgisruntime/toolkit/scalebar/style/renderer/GraduatedLineRenderer.kt
@@ -61,46 +61,48 @@ class GraduatedLineRenderer : ScalebarRenderer() {
         val segmentDisplayLength = lineDisplayLength / numSegments
 
         // Create Paint for drawing the ticks
-        paint.reset()
-        paint.style = Paint.Style.STROKE
-        paint.strokeWidth = lineWidthDp.dpToPixels(displayDensity).toFloat()
-        paint.strokeCap = Paint.Cap.ROUND
+        with(paint) {
+            reset()
+            style = Paint.Style.STROKE
+            strokeWidth = lineWidthDp.dpToPixels(displayDensity).toFloat()
+            strokeCap = Paint.Cap.ROUND
 
-        // Draw a tick, its shadow and a label at each segment boundary
-        var xPos = left + segmentDisplayLength
-        val yPos = top + (bottom - top) / 4 // segment ticks are 3/4 the height of the ticks at the start and end
-        val segmentDistance = distance / numSegments
-        val yPosText = bottom + textSizeDp.dpToPixels(displayDensity)
-        textPaint.textAlign = Paint.Align.CENTER
-        for (segNo in 1 until numSegments) {
-            // Draw the shadow, offset slightly from where the tick is drawn below
-            paint.color = shadowColor
-            canvas.drawLine(
-                xPos + SHADOW_OFFSET_PIXELS, yPos + SHADOW_OFFSET_PIXELS,
-                xPos + SHADOW_OFFSET_PIXELS, bottom + SHADOW_OFFSET_PIXELS, paint
-            )
+            // Draw a tick, its shadow and a label at each segment boundary
+            var xPos = left + segmentDisplayLength
+            val yPos = top + (bottom - top) / 4 // segment ticks are 3/4 the height of the ticks at the start and end
+            val segmentDistance = distance / numSegments
+            val yPosText = bottom + textSizeDp.dpToPixels(displayDensity)
+            textPaint.textAlign = Paint.Align.CENTER
+            for (segNo in 1 until numSegments) {
+                // Draw the shadow, offset slightly from where the tick is drawn below
+                color = shadowColor
+                canvas.drawLine(
+                    xPos + SHADOW_OFFSET_PIXELS, yPos + SHADOW_OFFSET_PIXELS,
+                    xPos + SHADOW_OFFSET_PIXELS, bottom + SHADOW_OFFSET_PIXELS, this
+                )
 
-            // Draw the line on top of the shadow
-            paint.color = lineColor
-            canvas.drawLine(xPos, yPos, xPos, bottom, paint)
+                // Draw the line on top of the shadow
+                color = lineColor
+                canvas.drawLine(xPos, yPos, xPos, bottom, this)
 
-            // Draw the label
-            canvas.drawText(ScalebarUtil.labelString(segmentDistance * segNo), xPos, yPosText, textPaint)
-            xPos += segmentDisplayLength
+                // Draw the label
+                canvas.drawText(ScalebarUtil.labelString(segmentDistance * segNo), xPos, yPosText, textPaint)
+                xPos += segmentDisplayLength
+            }
+
+            // Draw the line and its shadow, including the ticks at each end
+            drawLineAndShadow(canvas, left, top, right, bottom, lineWidthDp, lineColor, shadowColor)
+
+            // Draw a label at the start of the line
+            textPaint.textAlign = Paint.Align.LEFT
+            canvas.drawText("0", left, yPosText, textPaint)
+
+            // Draw a label at the end of the line
+            textPaint.textAlign = Paint.Align.RIGHT
+            canvas.drawText(ScalebarUtil.labelString(distance), right, yPosText, textPaint)
+            textPaint.textAlign = Paint.Align.LEFT
+            canvas.drawText(' ' + displayUnits.abbreviation, right, yPosText, textPaint)
         }
-
-        // Draw the line and its shadow, including the ticks at each end
-        drawLineAndShadow(canvas, left, top, right, bottom, lineWidthDp, lineColor, shadowColor)
-
-        // Draw a label at the start of the line
-        textPaint.textAlign = Paint.Align.LEFT
-        canvas.drawText("0", left, yPosText, textPaint)
-
-        // Draw a label at the end of the line
-        textPaint.textAlign = Paint.Align.RIGHT
-        canvas.drawText(ScalebarUtil.labelString(distance), right, yPosText, textPaint)
-        textPaint.textAlign = Paint.Align.LEFT
-        canvas.drawText(' ' + displayUnits.abbreviation, right, yPosText, textPaint)
     }
 
     override fun calculateExtraSpaceForUnits(displayUnits: LinearUnit?, textPaint: Paint): Float {
