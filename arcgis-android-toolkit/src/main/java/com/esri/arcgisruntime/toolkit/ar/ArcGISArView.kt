d8e4378c37f4ac0ad336321d3827e58cb92c3cf7
Luke William Duncan <lduncan@esri.com>
2019-08-14 10:00:38 +0100

    No longer stopping locationdatasource when setting.

--- arcgis-android-toolkit/src/main/java/com/esri/arcgisruntime/toolkit/ar/ArcGISArView.kt
+++ arcgis-android-toolkit/src/main/java/com/esri/arcgisruntime/toolkit/ar/ArcGISArView.kt
@@ -331,7 +331,6 @@ class ArcGISArView : FrameLayout, DefaultLifecycleObserver, Scene.OnUpdateListen
     var locationDataSource: LocationDataSource? = null
         set(value) {
             isTracking = if (value == null) {
-                field?.stop()
                 field?.removeLocationChangedListener(locationChangedListener)
                 field?.removeHeadingChangedListener(headingChangedListener)
                 field?.removeStatusChangedListener(locationDataSourceStatusChangedListener)
