ea1386f799b011497df330413b4f700a627b10e5
Luke William Duncan <lduncan@esri.com>
2019-08-20 10:50:11 +0100

    Refactoring comments.

--- arcgis-android-toolkit/src/main/java/com/esri/arcgisruntime/toolkit/ar/ArcGISArView.kt
+++ arcgis-android-toolkit/src/main/java/com/esri/arcgisruntime/toolkit/ar/ArcGISArView.kt
@@ -606,7 +606,7 @@ class ArcGISArView : FrameLayout, DefaultLifecycleObserver, Scene.OnUpdateListen
                         arCamera.displayOrientedPose.translation.map { it.toDouble() }.toDoubleArray()
                     )
                 }.let {
-                    // we must flip the y and z co-ordinates due to the compensation quaternion
+                    // swapping y and z co-ordinates and flipping the new y co-ordinate due to the compensation quaternion
                     TransformationMatrix.createWithQuaternionAndTranslation(
                         it.first.x.toDouble(),
                         it.first.y.toDouble(),
@@ -668,7 +668,7 @@ class ArcGISArView : FrameLayout, DefaultLifecycleObserver, Scene.OnUpdateListen
                 frame.hitTest(point.x.toFloat(), point.y.toFloat()).getOrNull(0).let { hitResult ->
                     hitResult?.let { theHitResult ->
                         theHitResult.hitPose.translation.map { it.toDouble() }.toDoubleArray().let {
-                            // we must flip the y and z co-ordinates due to the compensation quaternion
+                            // swapping y and z co-ordinates and flipping the new y co-ordinate due to the compensation quaternion
                             return TransformationMatrix.createWithQuaternionAndTranslation(
                                 0.0,
                                 0.0,
